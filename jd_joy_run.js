02a785d515367c426055de9e2c8e2ef8e3647190
huige <huige@protonmail.com>
2021-08-22 23:09:01 +0800

    update

--- jd_joy_run.js
+++ jd_joy_run.js
@@ -38,9 +38,12 @@ http-response ^https:\/\/draw\.jdfcloud\.com(\/mirror)?\/\/api\/user\/addUser\?c
 http-request ^https:\/\/draw\.jdfcloud\.com(\/mirror)?\/\/api\/user\/user\/detail\?openId= script-path=jd_joy_run.js, timeout=3600, tag=宠汪汪助力获取Token
 */
 const $ = new Env('宠汪汪赛跑');
-const zooFaker = require('./utils/JDJRValidator_Pure');
-$.get = zooFaker.injectToRequest2($.get.bind($));
-$.post = zooFaker.injectToRequest2($.post.bind($));
+// const zooFaker = require('./utils/JDJRValidator_Pure');
+// $.get = zooFaker.injectToRequest2($.get.bind($));
+// $.post = zooFaker.injectToRequest2($.post.bind($));
+import { injectToRequest2 } from './utils/JDJRValidator_Pure';
+$.get = injectToRequest2($.get.bind($));
+$.post = injectToRequest2($.post.bind($));
 //宠汪汪赛跑所需token，默认读取作者服务器的
 //需自行抓包，宠汪汪小程序获取token，点击`发现`或`我的`，寻找`^https:\/\/draw\.jdfcloud\.com(\/mirror)?\/\/api\/user\/user\/detail\?openId=`获取token
 let jdJoyRunToken = '';
@@ -595,7 +598,8 @@ isRequest ? getToken() : main();
 
 
 function taroRequest(e) {
-  const a = $.isNode() ? require('crypto-js') : CryptoJS;
+  // const a = $.isNode() ? require('crypto-js') : CryptoJS;
+  const a = require('crypto-js');
   const i = "98c14c997fde50cc18bdefecfd48ceb7"
   const o = a.enc.Utf8.parse(i)
   const r = a.enc.Utf8.parse("ea653f4f3c5eda12");
